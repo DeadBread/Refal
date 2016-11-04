a81a4fc9c943685bd90c7f85c03ab6fa0716e871
Vasiliy Zhikov <zhikov_n1@mail.ru>
2016-11-04 19:12:08 +0300

    just made some tests to make sure that refal compiler at least works.

---
+++ CMakeFiles/refal2.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/ArbitraryInteger.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/ArbitraryInteger.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/DirectiveParser.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/DirectiveParser.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/EmbeddedFunctions.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/EmbeddedFunctions.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Errors.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Errors.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Function.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Function.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/FunctionCompiler.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/FunctionCompiler.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/ModuleBuilder.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/ModuleBuilder.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/OperationsBuilder.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/OperationsBuilder.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/OperationsExecuter.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/OperationsExecuter.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Parser.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Parser.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Program.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Program.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Qualifier.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Qualifier.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/QualifierBuilder.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/QualifierBuilder.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/QualifierParser.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/QualifierParser.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Receptacle.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Receptacle.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Refal2.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Refal2.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/RuleParser.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/RuleParser.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Scanner.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Scanner.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Token.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Token.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Unit.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Unit.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/Variables.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/Variables.o"
+  "/home/kardamon/Documents/programs study/refal/refal-master/src/main.cpp" "/home/kardamon/Documents/programs study/refal/refal-master/CMakeFiles/refal2.dir/src/main.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/refal2.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/refal2.dir/src/ArbitraryInteger.o"
+  "CMakeFiles/refal2.dir/src/DirectiveParser.o"
+  "CMakeFiles/refal2.dir/src/EmbeddedFunctions.o"
+  "CMakeFiles/refal2.dir/src/Errors.o"
+  "CMakeFiles/refal2.dir/src/Function.o"
+  "CMakeFiles/refal2.dir/src/FunctionCompiler.o"
+  "CMakeFiles/refal2.dir/src/ModuleBuilder.o"
+  "CMakeFiles/refal2.dir/src/OperationsBuilder.o"
+  "CMakeFiles/refal2.dir/src/OperationsExecuter.o"
+  "CMakeFiles/refal2.dir/src/Parser.o"
+  "CMakeFiles/refal2.dir/src/Program.o"
+  "CMakeFiles/refal2.dir/src/Qualifier.o"
+  "CMakeFiles/refal2.dir/src/QualifierBuilder.o"
+  "CMakeFiles/refal2.dir/src/QualifierParser.o"
+  "CMakeFiles/refal2.dir/src/Receptacle.o"
+  "CMakeFiles/refal2.dir/src/Refal2.o"
+  "CMakeFiles/refal2.dir/src/RuleParser.o"
+  "CMakeFiles/refal2.dir/src/Scanner.o"
+  "CMakeFiles/refal2.dir/src/Token.o"
+  "CMakeFiles/refal2.dir/src/Unit.o"
+  "CMakeFiles/refal2.dir/src/Variables.o"
+  "CMakeFiles/refal2.dir/src/main.o"
+  "refal2.pdb"
+  "refal2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/refal2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
